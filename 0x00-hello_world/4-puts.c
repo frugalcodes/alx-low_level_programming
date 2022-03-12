11ae342969659b2e4b989e801c71466afbf969c1
frugalcodes <samueligwe63@gmail.com>
2022-03-12 22:04:04 +0100

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - code to put a message
+ * Return: Always 0
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
